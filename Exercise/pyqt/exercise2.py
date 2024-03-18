824509ec44d8e63d92c13bc94d449d7816ba624d
Raymart Bless C. Datuin <raymartd1@gmail.com>
2024-03-17 23:52:41 -0600

    Save this, need to make sure that virtualenvironment folder won't be saved. Need to know how

---
+++ Exercise/pyqt/exercise2.py
@@ -0,0 +1,19 @@
+import subprocess
+import time
+import win32gui
+
+
+def initUI(self):
+    # create a process
+    address_windows2 = "C:\\Users\\Rayma\\AppData\\Local\\BrainBay\\brainBay.exe"
+    subprocess.Popen(address_windows2)
+    hwnd = win32gui.FindWindowEx(0, 0, "brainbayFrame", "brainbay")
+    time.sleep(0.05)
+    window = QWindow.fromWinId(hwnd)
+    self.createWindowContainer(window, self)
+    self.setGeometry(500, 500, 450, 400)
+    self.setWindowTitle('File dialog')
+    self.show()
+
+
+## Get this to work...
---
+++ Exercise/pyqt/pip.bat
@@ -0,0 +1,1 @@
+@"C:\Python312\python.exe" "C:\Users\Rayma\AppData\Local\Programs\Python\Python37-32\Scripts\pip.exe" %*
